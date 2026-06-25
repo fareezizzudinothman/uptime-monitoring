4f2f956b824aa5ef89dbdc37d03f17135b72dd4a
FareezIO <fareezizzudin.othman@gmail.com>
2026-06-25 14:02:55 +0800

    addhealthroutes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const healthRoutes = require("./src/routes/health.routes")
+
 const app = express();
 
 const PORT = 3000;
@@ -7,6 +9,8 @@ const PORT = 3000;
 app.get("/", (req, res) => {
     res.send("🚀 Uptime Monitoring API is running!");
 });
+
+app.use("/api", healthRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
