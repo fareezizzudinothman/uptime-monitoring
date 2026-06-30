caa5ef276ad16d2bdfccc8fb5e6cafaffb40dc58
FareezIO <fareezizzudin.othman@gmail.com>
2026-06-30 16:14:57 +0800

    tambah healt controller

--- backend/src/controllers/health.controller.js
+++ backend/src/controllers/health.controller.js
@@ -1,10 +1,23 @@
-const healthCheck = (req, res) => {
-    res.json({
-        status: "OK",
-        message: "Uptime Monitoring API is running"
+const pool = require("../config/database");
+
+const checkDatabase = async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.status(200).json({
+      status: "OK",
+      database: "Connected",
+      serverTime: result.rows[0].now,
     });
+  } catch (error) {
+    res.status(500).json({
+      status: "ERROR",
+      database: "Disconnected",
+      error: error.message,
+    });
+  }
 };
 
 module.exports = {
-    healthCheck
+  checkDatabase,
 };
