ba9dd3a064f095741d066de86d8e0b524b760ee1
FareezIO <fareezizzudin.othman@gmail.com>
2026-06-30 15:56:48 +0800

    tambah database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  database: "uptime_monitor",
-  user: "postgres",
-  password: "postgres123",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 // Test database connection
